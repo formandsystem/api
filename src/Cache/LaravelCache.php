8187c1625e07613b44090bede7d80caa598218ce
Lukas Oppermann <lukas@vea.re>
2016-06-20 20:31:14 +0200

    adding Laravel Cache Service

---
+++ src/Cache/LaravelCache.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Formandsystem\Api\Cache;
+
+use Formandsystem\Api\Interfaces\Cache as CacheInterface;
+
+class LaravelCache implements CacheInterface
+{
+    protected $cache;
+
+    public function __construct(){
+        $this->cache = app('cache');
+    }
+
+    public function has($key){
+        return $this->cache->has($key);
+    }
+
+    public function get($key){
+        return $this->cache->get($key);
+    }
+
+    public function put($key, $value, $minutes = NULL){
+        return $this->cache->put($key, $value, $minutes);
+    }
+
+    public function forever($key, $value){
+        return $this->cache->forever($key, $value);
+    }
+
+    public function forget($key){
+        return $this->cache->forget($key);
+    }
+}
